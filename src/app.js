3233ece4b52a06aaa1f147ed765ac11145d86d91
cvincentc <59369148+cvincentc@users.noreply.github.com>
2021-06-30 22:31:12 -0700

    commit experiment

--- src/app.js
+++ src/app.js
@@ -54,8 +54,8 @@ app.get("/help/*",(req,res)=>{
     });
 });
 
-app.get("/weather",(req,res) =>{
-    if(!req.query.address){
+app.get("/weather",(req,res) =>{ 
+    if(!req.query.address){//s
         return res.send({ error: "You must provide an address"});
     }
     geocode(req.query.address,(error,{latitude, longitude, location} ={}) => {
